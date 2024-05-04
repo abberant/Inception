60877d66d306411775721f7f4abafb53ca24197e
abberant <abdoaoi112@gmail.com>
2024-05-03 19:30:33 -0500

    Done, at last. (later optimization might be required.)

--- srcs/requirements/mariadb/tools/install.sh
+++ srcs/requirements/mariadb/tools/install.sh
@@ -3,19 +3,23 @@
 service mariadb start
 
 # Create and import WordPress database
-mariadb -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
-mariadb -u root -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-mariadb -u root -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
-mariadb -u root -e "FLUSH PRIVILEGES;"
+mariadb -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+mariadb -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mariadb -u root -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
+mariadb -u root -p"$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
 
 # Set root password
-mariadb -u root -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');"
-mariadb -u root -e "FLUSH PRIVILEGES;"
+mariadb -u root -p"$MYSQL_ROOT_PASSWORD" -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');"
+mariadb -u root -p"$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
 
 # Allow root user to login from any host
-mariadb -u root -p $MYSQL_ROOT_PASSWORD "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-mariadb -u root -p $MYSQL_ROOT_PASSWORD "FLUSH PRIVILEGES;"
+mariadb -u root -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+
+mariadb -u root -p"$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
 
 # Stop MariaDB
-service mariadb stop
+# service mariadb stop
+echo "test2"
+mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
+echo "test1"
 mysqld
--- srcs/requirements/wordpress/tools/install.sh
+++ srcs/requirements/wordpress/tools/install.sh
@@ -4,7 +4,6 @@
 wget https://wordpress.org/latest.tar.gz
 tar -xvf latest.tar.gz
 mkdir -p /var/www/html/wordpress
-chmod 777 /var/www/html/wordpress
 mv wordpress/* /var/www/html/wordpress
 rm -rf latest.tar.gz
 
@@ -15,16 +14,9 @@ chown -R www-data:www-data /var/www/html/
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -O /usr/local/bin/wp
 chmod +x /usr/local/bin/wp
 
-wp core download --path=/var/www/html/wordpress --allow-root
-
-# Create wp-config
-wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST --allow-root
-
-wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
-
 # Configure WordPress
-# mv /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
-# wp config set DB_NAME $MYSQL_DATABASE --allow-root --path=/var/www/html/wordpress
-# wp config set DB_USER $MYSQL_USER --allow-root --path=/var/www/html/wordpress
-# wp config set DB_PASSWORD $MYSQL_PASSWORD --allow-root --path=/var/www/html/wordpress
-# wp config set DB_HOST $MYSQL_HOST --allow-root --path=/var/www/html/wordpress
+mv /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
+wp config set DB_NAME $MYSQL_DATABASE --allow-root --path=/var/www/html/wordpress
+wp config set DB_USER $MYSQL_USER --allow-root --path=/var/www/html/wordpress
+wp config set DB_PASSWORD $MYSQL_PASSWORD --allow-root --path=/var/www/html/wordpress
+wp config set DB_HOST mariadb --allow-root --path=/var/www/html/wordpress
